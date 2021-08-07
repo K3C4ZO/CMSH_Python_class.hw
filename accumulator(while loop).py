389b7df7cd58089696bf674e9cf7b866c74172bb
K3C4ZO <87697421+K3C4ZO@users.noreply.github.com>
2021-08-07 09:50:18 +0800

    Add files via upload

---
+++ accumulator(while loop).py
@@ -0,0 +1,9 @@
+def accumulator(x, y):
+    sum = 0
+    while x <= y:
+        sum = sum + x
+        x = x + 1
+    print(sum)
+############################
+
+#call funtion: accumulate(starting value, ending value)
