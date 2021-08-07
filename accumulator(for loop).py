3b4d3a7dd63c43b7fafea4d0fe483118d9648658
K3C4ZO <87697421+K3C4ZO@users.noreply.github.com>
2021-08-07 09:53:35 +0800

    Add files via upload

---
+++ accumulator(for loop).py
@@ -0,0 +1,8 @@
+def accumulator(x, y):
+    for sum in range (x, y+1, 1):
+        pass
+    print(sum)
+###########################
+
+#call funtion: accumulator(starting value, ending value)
+        
